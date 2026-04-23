475dc63d4ee26918e06dc54c7faa42f2579d2ada
Abdullah048 <abdullahshahbaz048@gmail.com>
2026-04-23 17:07:07 +0500

    feat: update serverExternalPackages to include 'jose' in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['jwks-rsa'],
+  serverExternalPackages: ['jwks-rsa', 'jose'],
   turbopack: {
     root: __dirname,
   },
